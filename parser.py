a82cdab8c1e1322bce112bb43e33cde94eadf182
Kostas Xanthopoulos <konxantho@gmail.com>
2020-12-15 18:44:15 +0200

    add Django creator call

--- parser.py
+++ parser.py
@@ -1,3 +1,7 @@
 from prance import ResolvingParser
+from Django.django_parser import DjangoCreator
 
 parser = ResolvingParser('test.yml')
+
+dc = DjangoCreator(parser.specification)
+dc.parse_endpoints()
