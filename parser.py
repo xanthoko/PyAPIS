cbc8d0ee827988abc8d130e1b78804ecca50deab
Kostas Xanthopoulos <konxantho@gmail.com>
2020-12-16 17:43:03 +0200

    ⚙️  Add a simple test yml file

--- parser.py
+++ parser.py
@@ -1,7 +1,7 @@
 from prance import ResolvingParser
 from Django.django_parser import DjangoCreator
 
-parser = ResolvingParser('test.yml')
+parser = ResolvingParser('test_simple.yml')
 
 dc = DjangoCreator(parser.specification)
 dc.parse_endpoints()
