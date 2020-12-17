d1337cdd6856ed94cd37f1dc41796504c581f80b
Kostas Xanthopoulos <konxantho@gmail.com>
2020-12-17 14:33:24 +0200

    💡 Add command line argument for framework decleration and Creation of django project with os.system

--- parser.py
+++ parser.py
@@ -1,7 +1,44 @@
+import os
+import argparse
+
 from prance import ResolvingParser
+
+from file_paths import DJANGO_PROJECT_PATH
 from Django.django_parser import DjangoCreator
 
-parser = ResolvingParser('test_simple.yml')
 
-dc = DjangoCreator(parser.specification)
-dc.parse_endpoints()
+def get_framework_from_arguments():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('framework', help="The framework to create the API")
+
+    args = arg_parser.parse_args()
+    return args.framework
+
+
+def generate_django_API():
+    if not os.path.exists(DJANGO_PROJECT_PATH):
+        project_name = DJANGO_PROJECT_PATH.split('/')[-1]
+        os.chdir('Django')
+        os.system(f'django-admin startproject {project_name}')
+        os.chdir('..')
+        print('[INFO] Django project created')
+
+    dc = DjangoCreator(yml_parser.specification)
+    dc.parse_endpoints()
+    print('[INFO] Djang API generated')
+
+
+def validate_given_framework(framework):
+    supported_frameworks = ['django']
+    if framework not in supported_frameworks:
+        print(f'[ERROR] "{framework}" is not a supported framework')
+        exit()
+
+
+if __name__ == '__main__':
+    yml_parser = ResolvingParser('test_simple.yml')
+    framework = get_framework_from_arguments()
+    validate_given_framework(framework)
+
+    if framework == 'django':
+        generate_django_API()
